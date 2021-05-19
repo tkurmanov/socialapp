6e0c1cb2c8cebd2bdc3204eba29bee49a08c4a63
Nomad <nomad@Nomads-MacBook-Pro.local>
2021-05-19 20:20:41 +0600

    content and navigator

---
+++ __tests__/App.js
@@ -0,0 +1,10 @@
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+  // Timur Kurmanov 20MD0188
+});
